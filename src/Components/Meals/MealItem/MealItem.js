b1343265a689d55b87048e0ac61ad8c434eefde7
AshishSinghDeveloper <ashishsng21@hotmail.com>
2022-03-14 19:33:30 -0500

    Add meal list

---
+++ src/Components/Meals/MealItem/MealItem.js
@@ -0,0 +1,24 @@
+import style from "./MealItem.module.css";
+
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li className={style.meal}>
+        <div>
+          <h3>{props.name}</h3>
+          <div className={style.description}>{props.description}</div>
+          <div className={style.price}>{price}</div>
+        </div>
+        <div>
+          <div>
+            Amount
+          </div>
+          <div>
+            <button>Add</button>
+          </div>
+        </div>
+    </li>
+  );
+};
+
+export default MealItem;
