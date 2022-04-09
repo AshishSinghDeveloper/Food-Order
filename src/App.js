33198e1f3f680d52ff565dcbe16a7f880d396858
AshishSinghDeveloper <ashishsng21@hotmail.com>
2022-04-09 16:38:57 -0500

    Add Cart Context and provider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
+import CartProvider from "./Store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
   const showCartHandler = () => {
     setCartIsShown(true);
-  }
+  };
 
   const hideCartHandler = () => {
     setCartIsShown(false);
-  }
+  };
   return (
-    <Fragment>
-      {cartIsShown &&  <Cart onHideCart={hideCartHandler}/>}
-      
-      <Header onShowCart={showCartHandler}/>
+    <CartProvider>
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
